20e54c3d8469b6225d9a556e3b48f65648b4b17b
sandy1618 <sandeepnayak1.618@gmail.com>
2024-07-22 16:20:31 +0900

    [Add] Inital starter code for repository publishing github action

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Sandeep's Git Tutorial
+
+This is a sample Python package for tutorial purposes.
+
+
 #### I am trying to use this for learning github 
 
 	The first Tutorial is to see if i can push local repo to remote without having the remote repo name same as local repo folder name ...
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sandeeps_git_tutorial',
+    version='0.1.0',
+    description='A sample Python package for tutorial purposes',
+    author='Sandeep',
+    author_email='sandeep@example.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
